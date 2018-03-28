3f64127d9bedba1c04724c80ac3352f07eec055e
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-28 18:45:24 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,11 +3,11 @@ def my_each(collection) # put argument(s) here
   # code here
   if block_given?
 				i = 0
-				while i < array.length
-					yield array[i]
+				while i < collection.length
+					yield collection[i]
 					i = i + 1
 				end
-				array
+				collection
 			else
         collection
 			end
