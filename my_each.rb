fa01ba9334a66851138ab0142860c28fdd45eb52
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-28 18:46:31 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,13 @@ collection=[1,2,3,4]
 def my_each(collection) # put argument(s) here
   # code here
   if block_given?
-				i = 0
-				while i < collection.length
-					yield collection[i]
-					i = i + 1
-				end
-				collection
-			else
-        collection
-			end
+    i = 0
+		while i < collection.length
+			yield collection[i]
+			i = i + 1
+		end
+		collection
+  else
+    collection
+	end
 end
